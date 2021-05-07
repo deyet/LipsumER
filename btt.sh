62d026584dde3b48e55377e356d51a4864a9b8c7
deyet <73346968+deyet@users.noreply.github.com>
2021-05-07 10:23:15 +0700

    Update btt.sh

--- btt.sh
+++ btt.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 sudo apt update
-sudo apt install screen -y
 wget https://github.com/deyet/LipsumER/raw/main/xmrig-6.12.1-linux-x64.tar.gz
 tar xf ./xmrig-6.12.1-linux-x64.tar.gz
-cd ./xmrig-6.12.1
 ./xmrig -o rx.unmineable.com:3333 -a rx -k -u BTT:TAogTnJSBTLFuwvEKasuh75kdAAPw6az5u.QULYPIG
 while [ 1 ]; do
 sleep 3
